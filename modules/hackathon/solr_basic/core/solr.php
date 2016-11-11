cb6c13c234534e5d3391598c9a73cba7e5ff2111
Ralf Kronen <ralf.kronen@daylaborers.de>
2016-11-11 17:15:29 +0100

    Solr Core File for Updates in Crons or Console

---
+++ modules/hackathon/solr_basic/core/solr.php
@@ -0,0 +1,7 @@
+<?php
+
+class solr extends oxBase
+{
+
+
+} //class
--- modules/hackathon/solr_basic/metadata.php
+++ modules/hackathon/solr_basic/metadata.php
@@ -1,15 +1,19 @@
 <?php
 
 $aModule = array(
-    'id' => 'solr_basic',
-    'title' => 'Solr für Oxid',
-    'description' => 'Solr-Suche für Oxid',
-    'author' => 'Oxid Hackthon',
-    'version' => '1.0',
-    'extend'      => array(
-        'oxsearch' => 'hackathon/solr_basic/modules/solr_oxsearch',
+    'id'            => 'solr_basic',
+    'title'         => 'Solr für Oxid',
+    'description'   => 'Solr-Suche für Oxid',
+    'author'        => 'Oxid Hackthon',
+    'version'       => '1.0',
+    'extend'        => array(
+        'oxsearch'  => 'hackathon/solr_basic/models/solr_oxsearch',
+    ),
+    'files' => array(
+        'solr'      => 'hackathon/solr_basic/core/solr.php',
     ),
     'blocks' => array(
-       // array('template' => 'page/list/list.tpl', 'block'=>'page_list_listhead', 'file'=> '/views/blocks/page_list_listhead.tpl'),
+        // array('template' => 'page/list/list.tpl', 'block'=>'page_list_listhead', 'file'=> '/views/blocks/page_list_listhead.tpl'),
     ),
+
 );
