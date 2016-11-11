043a78d95de96d2fceefcc0d3b1b7c515069762f
Kathrin <Kathrin.Welzel@mgm-tp.com>
2016-11-11 19:08:35 +0100

    -new README -added module settings

--- modules/hackathon/solr_basic/metadata.php
+++ modules/hackathon/solr_basic/metadata.php
@@ -1,18 +1,43 @@
 <?php
 
 $aModule = array(
-    'id'            => 'solr_basic',
-    'title'         => 'Solr für Oxid',
-    'description'   => 'Solr-Suche für Oxid',
-    'author'        => 'Oxid Hackthon',
-    'version'       => '1.0',
-    'extend'        => array(
-        'oxsearch'  => 'hackathon/solr_basic/models/solr_oxsearch',
-    ),
-    'files' => array(
-        'solr'      => 'hackathon/solr_basic/core/solr.php',
-    ),
-    'blocks' => array(
-        // array('template' => 'page/list/list.tpl', 'block'=>'page_list_listhead', 'file'=> '/views/blocks/page_list_listhead.tpl'),
-    ),
+        'id' => 'solr_basic',
+        'title' => 'Solr für Oxid',
+        'description' => 'Solr-Suche für Oxid',
+        'author' => 'Oxid Hackthon',
+        'version' => '1.0',
+        'extend' => array(
+                'oxsearch' => 'hackathon/solr_basic/models/solr_oxsearch',
+        ),
+        'files' => array(
+                'solr' => 'hackathon/solr_basic/core/solr.php',
+        ),
+        'blocks' => array(// array('template' => 'page/list/list.tpl', 'block'=>'page_list_listhead', 'file'=> '/views/blocks/page_list_listhead.tpl'),
+        ),
+        'settings' => array(
+                array(
+                        'group' => 'SOLR_CONFIG',
+                        'name' => 'CORE',
+                        'type' => 'str',
+                        'value' => ''
+                ),
+                array(
+                        'group' => 'SOLR_CONFIG',
+                        'name' => 'HOST',
+                        'type' => 'str',
+                        'value' => ''
+                ),
+                array(
+                        'group' => 'SOLR_CONFIG',
+                        'name' => 'PORT',
+                        'type' => 'str',
+                        'value' => ''
+                ),
+                array(
+                        'group' => 'SOLR_CONFIG',
+                        'name' => 'PATH',
+                        'type' => 'str',
+                        'value' => ''
+                ),
+        )
 );
